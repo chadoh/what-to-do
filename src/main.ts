08f14a6b8c7a764c957edd961b799df12c2401fc
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2021-12-31 14:11:02 -0500

    feat: add "nah" button
    also improve TS and address linting issues

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,14 @@
-const defaultActivities = `watching TV
+const suggestionEl = document.querySelector('#activity')
+const textarea = document.querySelector('textarea')
+const saveButton = document.querySelector('#save')
+const editButton = document.querySelector('#edit')
+const editArea = document.querySelector('#edit-area')
+const againButton = document.querySelector('#again') as HTMLElement
+if (!suggestionEl || !textarea || !saveButton || !editButton || !againButton || !editArea) {
+  throw new Error('cannot find expected DOM elements')
+}
+
+const defaultPossibilities = `watching TV
 reading to each other
 learning to play a song together
 drawing maps from memory or imagination
@@ -6,33 +16,44 @@ cooking something fun
 build kites; go fly them
 yoga`
 
-let activities = localStorage.getItem('activities')
-if (!activities) {
-  activities = defaultActivities
+const LOCAL_STORAGE_KEY = 'activities'
+
+function getAll({ raw = false } = {}) {
+  const possibilities = localStorage.getItem(LOCAL_STORAGE_KEY) || defaultPossibilities
+  if (raw) return possibilities
+  return possibilities.split('\n')
 }
 
-const suggestion = document.querySelector('#activity')
+function suggest() {
+  againButton.style.display = 'none'
+  suggestionEl.closest('h1').classList.add('loader')
 
-if (suggestion) {
-  const array = activities.split('\n')
-  const randomActivity = array[Math.floor(Math.random() * array.length)]
-  suggestion.innerHTML = randomActivity
+  const possibilities = getAll()
+  const suggestion = possibilities[
+    Math.floor(Math.random() * possibilities.length)
+  ]
+
+  setTimeout(() => {
+    suggestionEl.closest('h1').classList.remove('loader')
+    suggestionEl.innerHTML = suggestion
+    againButton.style.display = null
+  }, 750)
 }
 
-const textarea = document.querySelector('textarea')
-const saveButton = document.querySelector('#save')
-const editButton = document.querySelector('#edit')
-const editArea = document.querySelector('#edit-area')
-if (editButton && editArea && textarea && saveButton) {
-  editButton.addEventListener('click', event => {
-    const editing = editArea.className === 'active'
-    editArea.className = editing ? '' : 'active'
-    if (!editing) textarea.focus()
-  })
-
-  textarea.value = activities
-  saveButton.addEventListener('click', event => {
-    localStorage.setItem('activities', textarea.value)
-    window.location.reload(false)
-  })
+function toggleEditor() {
+  const editing = editArea.className === 'active'
+  editArea.className = editing ? '' : 'active'
+  if (!editing) textarea.focus()
 }
+
+textarea.value = getAll({ raw: true }) as string
+saveButton.addEventListener('click', () => {
+  localStorage.setItem(LOCAL_STORAGE_KEY, textarea.value)
+  toggleEditor()
+  suggest()
+})
+
+againButton.addEventListener('click', suggest)
+editButton.addEventListener('click', toggleEditor)
+
+suggest()
